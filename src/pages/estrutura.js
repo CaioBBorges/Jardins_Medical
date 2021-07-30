8c19b0465164f98e4a5637f94205371d516b0973
Caio Rafael Rocha de Andrade <76894052+Caiocr8@users.noreply.github.com>
2021-07-30 19:47:44 -0300

    ESTRUTURA (FALTA A ULTIMA SECTION)

--- src/pages/estrutura.js
+++ src/pages/estrutura.js
@@ -6,6 +6,9 @@ import ola1 from "../images/ola1.svg"
 import inovador from "../images/inovador.svg"
 import bg4 from "../images/bg4.png"
 import Header from "../components/header"
+import bg3 from "../images/bg3.png"
+
+
 
 const estrutura = () => (
   <div class="">
@@ -52,13 +55,13 @@ const estrutura = () => (
           width: `1244px`,
           height: `243px`,
           left: `123px`,
-          top: `2508px`,
           background: ` rgba(255, 255, 255, 0.02)`,
-          backdropFilter: `blur(15px)`,
+          backdropFilter: `blur(3px)`,
           borderRadius: `59px`,
         }}
       >
         <p
+          class="text-center p-12"
           style={{
             fontSize: `18px`,
             lineHeight: `171.2%`,
@@ -68,11 +71,133 @@ const estrutura = () => (
             textShadow: `0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 7px rgba(0, 0, 0, 0.8), 4px 9px 34px rgba(0, 0, 0, 0.8)`,
           }}
         >
-          EM QUASE EM SEUS 5700,00m² de área O JARDINS MEDICAL OFERECE UMA
+          EM QUASE EM SEUS 5700,00m² DE ÁREA O JARDINS MEDICAL OFERECE UMA
           ESTRUTURA COMPLETA PARA SUA ROTINA COM TODOS OS ASPECTOS FACILITIES ,
           VOCÊ TEM TUDO O QUE PRECISA PARA COMPOR UMA SEDE MÉDICA CLASSE (AA) EM
           SERVIÇO E ATENDIMENTO.
         </p>
+      </div>
+    </section>
+    <section
+      class="bg-cover bg-center bg-local bg-no-repeat h-screen"
+      style={{
+        backgroundImage: `url(${bg3})`,
+      }}
+    > 
+      
+      <div class="grid grid-cols-4 justify-between px-16 place-items-center pt-20"> 
+      { /* PRIMEIRO COMPONENTE  */ }
+        <div>
+          <StaticImage
+              src="../images/lobby_e.svg"
+              quality={100}
+              placeholder="blurred"
+              layout="fixed"
+            />   
+          <div
+            style={{
+              width: `307px`,
+              height: `250px`,
+              left: `61px`,
+              top: `3349px`, 
+              background: `rgba(255, 255, 255, 0.1)`,
+              backdropFilter: `blur(20px)`,
+            }}
+            >
+              <h1 class="  pt-5 text-center text-lg font-bold text-white font-mono">LOBBY</h1>
+              <p class="pt-5 px-5 font-thin text-white text-sm leading-relaxed tracking-widest font-mono ">LOBBY PERFEITO PARA RECEBER TODO O CORPO MÉDICO E OS PACIENTES, IDEAL PARA INFORMAR, GUIAR E ORIENTAR AS PESSOAS PROCURANDO POR CONSULTÓRIOS.</p>
+          </div>          
+        </div>
+
+        { /* SEGUNDA COMPONENTE  */ }
+        <div >
+          <StaticImage
+              src="../images/salas_cirurgia.svg"
+              quality={100}
+              placeholder="blurred"
+              layout="fixed"
+            />   
+          <div
+            style={{
+              width: `307px`,
+              height: `250px`,
+              left: `61px`,
+              top: `3349px`, 
+              background: `rgba(255, 255, 255, 0.1)`,
+              backdropFilter: `blur(20px)`,
+            }}
+            >
+              <h1 class=" px-5 pt-5 text-lg text-center font-bold text-white font-mono">SALAS PARA CENTROS <br /> CIRUGICOS </h1>
+              <p class="pt-5 px-5 font-thin text-white text-sm leading-relaxed tracking-widest font-mono ">O JARDINS MEDICAL, OFERECE UMA ESTRUTURA IDEAL PARA CENTROS CENTROS CIRURGICOS E SALA DE OPERAÇÕES.</p>
+          </div>          
+        </div>
+        { /* TERCEIRO COMPONENTE  */ }
+        <div>
+          <StaticImage
+              src="../images/rooftop_e.svg"
+              quality={100}
+              placeholder="blurred"
+              layout="fixed"
+            />   
+          <div
+            style={{
+              width: `307px`,
+              height: `250px`,
+              left: `61px`,
+              top: `3349px`, 
+              background: `rgba(255, 255, 255, 0.1)`,
+              backdropFilter: `blur(20px)`,
+            }}
+            >
+              <h1 class=" pt-5 text-center text-lg font-bold text-white font-mono">ROOFTOP</h1>
+              <p class="pt-5 px-5 font-thin text-white text-sm leading-relaxed tracking-widest font-mono ">UM AMBIENTE CONFORTÁVEL E ACONCHEGANTE, IDEAL PARA OFERECER MOMENTOS DE TRANQUILIDADE A TODOS OS PACIENTES E TERCEIROS.</p>
+          </div>          
+        </div>
+        { /* QUARTO COMPONENTE  */ }
+        <div>
+          <StaticImage
+              src="../images/auditorio_e.svg"
+              quality={100}
+              placeholder="blurred"
+              layout="fixed"
+            />   
+          <div
+            style={{
+              width: `307px`,
+              height: `250px`,
+              left: `61px`,
+              top: `3349px`, 
+              background: `rgba(255, 255, 255, 0.1)`,
+              backdropFilter: `blur(20px)`,
+            }}
+            >
+              <h1 class=" pt-5 text-center text-lg font-bold text-white font-mono">AUDITÓRIO</h1>
+              <p class="pt-5 px-5 font-thin text-white text-sm leading-relaxed tracking-widest font-mono ">UM LOCAL PARA INCENTIVAR PALESTRAS E REUNIÕES DE PESQUISA E ATUAÇÕES DOS PROFISSIONAIS DA ÁREA DA SAÚDE.</p>
+          </div>          
+        </div>
+      </div>
+    </section>
+    <section
+      class="bg-cover bg-center bg-no-repeat w-full h-5/6  justify-center bg-clip-border"
+      style={{
+        backgroundImage: `url(${bg4})`,
+      }}
+    >
+      <h1
+        class="text-white font-mono text-9xl text-center font-thin	pt-80 w-full"
+        style={{
+          textAlign: `center`,
+          letterSpacing: `0.63em`,
+          color: `#`,
+          textShadow: `8px 13px 17px rgba(0, 0, 0, 0.55)`,
+        }}
+      >
+        A MELHOR <br /> EXPERIÊNCIA
+      </h1>
+      <div class=" ml-64">
+        <button class="bg-green-dark ml-96 rounded-full h-20 text-white w-1/3 text-center font-mono p-3 mt-20 mb-80">
+          SAIBA MAIS
+        </button>
       </div>
     </section>
   </div>
