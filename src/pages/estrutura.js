02437d696c9999282221fc5f7cb9633afd9bcbf9
caiobraga <caio.braga.brasil@plugwithus.pt>
2021-07-30 21:17:26 -0300

    Localizacao

--- src/pages/estrutura.js
+++ src/pages/estrutura.js
@@ -8,8 +8,6 @@ import bg4 from "../images/bg4.png"
 import Header from "../components/header"
 import bg3 from "../images/bg3.png"
 
-
-
 const estrutura = () => (
   <div class="">
     <section
@@ -25,7 +23,7 @@ const estrutura = () => (
       <Header />
       <div class="pb-98">
         <h1
-        class="text-center w-full p-56"
+          class="text-center w-full p-56"
           style={{
             fontSize: `36px`,
             lineHeight: `171.2%`,
@@ -55,7 +53,7 @@ const estrutura = () => (
           width: `1244px`,
           height: `243px`,
           left: `123px`,
-          background: ` rgba(255, 255, 255, 0.02)`,
+          background: ` rgba(200, 200, 200, 0.05)`,
           backdropFilter: `blur(3px)`,
           borderRadius: `59px`,
         }}
@@ -71,7 +69,7 @@ const estrutura = () => (
             textShadow: `0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 7px rgba(0, 0, 0, 0.8), 4px 9px 34px rgba(0, 0, 0, 0.8)`,
           }}
         >
-          EM QUASE EM SEUS 5700,00m² DE ÁREA O JARDINS MEDICAL OFERECE UMA
+          EM SEUS QUASE 5700,00m² DE ÁREA O JARDINS MEDICAL OFERECE UMA
           ESTRUTURA COMPLETA PARA SUA ROTINA COM TODOS OS ASPECTOS FACILITIES ,
           VOCÊ TEM TUDO O QUE PRECISA PARA COMPOR UMA SEDE MÉDICA CLASSE (AA) EM
           SERVIÇO E ATENDIMENTO.
@@ -83,97 +81,117 @@ const estrutura = () => (
       style={{
         backgroundImage: `url(${bg3})`,
       }}
-    > 
-      
-      <div class="grid grid-cols-4 justify-between px-16 place-items-center pt-20"> 
-      { /* PRIMEIRO COMPONENTE  */ }
+    >
+      <div class="grid grid-cols-4 justify-between px-16 place-items-center pt-20">
+        {/* PRIMEIRO COMPONENTE  */}
         <div>
           <StaticImage
-              src="../images/lobby_e.svg"
-              quality={100}
-              placeholder="blurred"
-              layout="fixed"
-            />   
+            src="../images/lobby_e.svg"
+            quality={100}
+            placeholder="blurred"
+            layout="fixed"
+          />
           <div
             style={{
               width: `307px`,
               height: `250px`,
               left: `61px`,
-              top: `3349px`, 
+              top: `3349px`,
               background: `rgba(255, 255, 255, 0.1)`,
               backdropFilter: `blur(20px)`,
             }}
-            >
-              <h1 class="  pt-5 text-center text-lg font-bold text-white font-mono">LOBBY</h1>
-              <p class="pt-5 px-5 font-thin text-white text-sm leading-relaxed tracking-widest font-mono ">LOBBY PERFEITO PARA RECEBER TODO O CORPO MÉDICO E OS PACIENTES, IDEAL PARA INFORMAR, GUIAR E ORIENTAR AS PESSOAS PROCURANDO POR CONSULTÓRIOS.</p>
-          </div>          
+          >
+            <h1 class="pt-5 text-center text-lg font-bold text-white font-mono">
+              LOBBY
+            </h1>
+            <p class="pt-5 px-5 font-thin text-white text-sm leading-relaxed tracking-widest font-mono ">
+              LOBBY PERFEITO PARA RECEBER TODO O CORPO MÉDICO E OS PACIENTES,
+              IDEAL PARA INFORMAR, GUIAR E ORIENTAR AS PESSOAS PROCURANDO POR
+              CONSULTÓRIOS.
+            </p>
+          </div>
         </div>
 
-        { /* SEGUNDA COMPONENTE  */ }
-        <div >
-          <StaticImage
-              src="../images/salas_cirurgia.svg"
-              quality={100}
-              placeholder="blurred"
-              layout="fixed"
-            />   
-          <div
-            style={{
-              width: `307px`,
-              height: `250px`,
-              left: `61px`,
-              top: `3349px`, 
-              background: `rgba(255, 255, 255, 0.1)`,
-              backdropFilter: `blur(20px)`,
-            }}
-            >
-              <h1 class=" px-5 pt-5 text-lg text-center font-bold text-white font-mono">SALAS PARA CENTROS <br /> CIRUGICOS </h1>
-              <p class="pt-5 px-5 font-thin text-white text-sm leading-relaxed tracking-widest font-mono ">O JARDINS MEDICAL, OFERECE UMA ESTRUTURA IDEAL PARA CENTROS CENTROS CIRURGICOS E SALA DE OPERAÇÕES.</p>
-          </div>          
-        </div>
-        { /* TERCEIRO COMPONENTE  */ }
+        {/* SEGUNDA COMPONENTE  */}
         <div>
           <StaticImage
-              src="../images/rooftop_e.svg"
-              quality={100}
-              placeholder="blurred"
-              layout="fixed"
-            />   
+            src="../images/salas_cirurgia.svg"
+            quality={100}
+            placeholder="blurred"
+            layout="fixed"
+          />
           <div
             style={{
               width: `307px`,
               height: `250px`,
               left: `61px`,
-              top: `3349px`, 
+              top: `3349px`,
               background: `rgba(255, 255, 255, 0.1)`,
               backdropFilter: `blur(20px)`,
             }}
-            >
-              <h1 class=" pt-5 text-center text-lg font-bold text-white font-mono">ROOFTOP</h1>
-              <p class="pt-5 px-5 font-thin text-white text-sm leading-relaxed tracking-widest font-mono ">UM AMBIENTE CONFORTÁVEL E ACONCHEGANTE, IDEAL PARA OFERECER MOMENTOS DE TRANQUILIDADE A TODOS OS PACIENTES E TERCEIROS.</p>
-          </div>          
+          >
+            <h1 class=" px-5 pt-5 text-lg text-center font-bold text-white font-mono">
+              SALAS PARA CENTROS <br /> CIRUGICOS{" "}
+            </h1>
+            <p class="pt-5 px-5 font-thin text-white text-sm leading-relaxed tracking-widest font-mono ">
+              O JARDINS MEDICAL, OFERECE UMA ESTRUTURA IDEAL PARA CENTROS
+              CENTROS CIRURGICOS E SALA DE OPERAÇÕES.
+            </p>
+          </div>
         </div>
-        { /* QUARTO COMPONENTE  */ }
+        {/* TERCEIRO COMPONENTE  */}
         <div>
           <StaticImage
-              src="../images/auditorio_e.svg"
-              quality={100}
-              placeholder="blurred"
-              layout="fixed"
-            />   
+            src="../images/rooftop_e.svg"
+            quality={100}
+            placeholder="blurred"
+            layout="fixed"
+          />
           <div
             style={{
               width: `307px`,
               height: `250px`,
               left: `61px`,
-              top: `3349px`, 
+              top: `3349px`,
               background: `rgba(255, 255, 255, 0.1)`,
               backdropFilter: `blur(20px)`,
             }}
-            >
-              <h1 class=" pt-5 text-center text-lg font-bold text-white font-mono">AUDITÓRIO</h1>
-              <p class="pt-5 px-5 font-thin text-white text-sm leading-relaxed tracking-widest font-mono ">UM LOCAL PARA INCENTIVAR PALESTRAS E REUNIÕES DE PESQUISA E ATUAÇÕES DOS PROFISSIONAIS DA ÁREA DA SAÚDE.</p>
-          </div>          
+          >
+            <h1 class=" pt-5 text-center text-lg font-bold text-white font-mono">
+              ROOFTOP
+            </h1>
+            <p class="pt-5 px-5 font-thin text-white text-sm leading-relaxed tracking-widest font-mono ">
+              UM AMBIENTE CONFORTÁVEL E ACONCHEGANTE, IDEAL PARA OFERECER
+              MOMENTOS DE TRANQUILIDADE A TODOS OS PACIENTES E TERCEIROS.
+            </p>
+          </div>
+        </div>
+        {/* QUARTO COMPONENTE  */}
+        <div>
+          <StaticImage
+            src="../images/auditorio_e.svg"
+            quality={100}
+            placeholder="blurred"
+            layout="fixed"
+          />
+          <div
+            style={{
+              width: `307px`,
+              height: `250px`,
+              left: `61px`,
+              top: `3349px`,
+              background: `rgba(255, 255, 255, 0.1)`,
+              backdropFilter: `blur(20px)`,
+            }}
+          >
+            <h1 class=" pt-5 text-center text-lg font-bold text-white font-mono">
+              AUDITÓRIO
+            </h1>
+            <p class="pt-5 px-5 font-thin text-white text-sm leading-relaxed tracking-widest font-mono ">
+              UM LOCAL PARA INCENTIVAR PALESTRAS E REUNIÕES DE PESQUISA E
+              ATUAÇÕES DOS PROFISSIONAIS DA ÁREA DA SAÚDE.
+            </p>
+          </div>
         </div>
       </div>
     </section>
@@ -184,8 +202,10 @@ const estrutura = () => (
       }}
     >
       <h1
-        class="text-white font-mono text-9xl text-center font-thin	pt-80 w-full"
+        class="text-white font-mono font-thin pt-80 w-full text-8xl"
         style={{
+          fontSize: `48px`,
+          lineHeight: `171.2%`,
           textAlign: `center`,
           letterSpacing: `0.63em`,
           color: `#`,
@@ -194,8 +214,13 @@ const estrutura = () => (
       >
         A MELHOR <br /> EXPERIÊNCIA
       </h1>
-      <div class=" ml-64">
-        <button class="bg-green-dark ml-96 rounded-full h-20 text-white w-1/3 text-center font-mono p-3 mt-20 mb-80">
+      <div
+        class="flex
+      justify-center
+      "
+        style={{ align: `center` }}
+      >
+        <button class="bg-green-dark rounded-full h-20 text-white w-1/3 text-center font-mono p-3 mt-20 mb-80">
           SAIBA MAIS
         </button>
       </div>
--- src/pages/localizacao.js
+++ src/pages/localizacao.js
@@ -1,160 +1,70 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-import bg3 from "../images/bg3.png"
-import ola1 from "../images/ola1.svg"
-import inovador from "../images/inovador.svg"
-import bg4 from "../images/bg4.png"
+import lobby from "../images/lobby.png"
 import Header from "../components/header"
+import tablet from "../images/tablet.png"
 
 const localizacao = () => (
   <div class=" ">
+    <title>JARDINS</title>
+    <link rel="icon" href="..\images\favicon.ico" />
     <section
-      class="bg-cover bg-local bg-no-repeat	bg-clip-border h-full place-content-center"
-      style={{ backgroundImage: `url(${inovador})` , resize: `both`,
-      overflow: `hidden`}}
-    >
-      <title>JARDINS</title>
-      <link rel="icon" href="..\images\favicon.ico" />
-      <Header/>
-      <div class="">
-        <h1 class="pt-36 w-full text-8xl text-white font-mono font-thin text-center px-24 pb-100">
-          ESTRUTURA PERFEITA PARA VOCÊ FOCAR NO QUE REALMENTE IMPORTA:<br/>SEU
-          PACIENTE.
-        </h1>
-      </div>
-    </section>
-    <section class="grid grid-cols-2 w-full h-screen px-10 bg-gradient-to-tl from-gray-300 via-gray-50 to-gray-50 items-center">
-      <div class="">
-        <StaticImage src="../images/Mask Group.png" class="" quality={100} />
-      </div>
-      <div class="w">
-        <h2 class="text-7xl md:text-5xl sm:text-4xl xs:text-4xl leading-relaxed tracking-widest font-mono text-black text-right">
-          UM NOVO PADRÃO DE ATENDIMENTO MÉDICO
-        </h2>
-        <p class="text-2xl md:text-xl sm:text-sm xs:text-sm break-words mt-14 font-mono text-center">
-          O Jardins Medical traz um novo conceito de excelência, com espaços
-          amplos de atendimento, localização privilegiada E arquitetura
-          inovadora, Contará com profissionais que são referência em suas áreas.
-          todo projeto busca o melhor resultado para o paciente, e também para o
-          profissional de saúde. Estarão disponíveis para venda salas para uso
-          exclusivo da área de saúde.
-        </p>
-      </div>
-    </section>
-
-    <section
-      class="bg-cover bg-center bg-local bg-no-repeat	 grid grid-cols-2"
+      class="bg-cover bg-local h-full"
       style={{
-        backgroundImage: `url(${bg3})`,
+        backgroundImage: `url(${lobby})`,
       }}
     >
-      <div
-        class="text-white font-mono col-2 grid grid-cols-4 grid-rows-5 col-start-2 -mt-6 p-6 mx-20 place-content-center	place-items-center flex-shrink"
-        src="../images/inovador.svg"
-        style={{
-          backgroundImage: `url(${ola1})`,
-          backgroundRepeat: `no-repeat`,
-          height: `100vh`,
-          weight: `10vw`,
-          backgroundAttachment: `local`,
-          backgroundSize: `100%`,
-        }}
-      >
-        <h1 class="text-white text-5xl w-full col-span-4 p-5 text-justify">
-          O QUE O JARDINS MEDICAL OFERECE PARA VOCÊ?
-        </h1>
-        <StaticImage
-          src="../images/compass.png"
-          quality={100}
-          placeholder="blurred"
-          layout="fixed"
-        />
-        <div class="text-white">
-          <h1 class="text-lg font-mono">5635,26</h1>
-          <h2 class="text-sm font-mono pt-2">PARA SALAS MEDICAS</h2>
+      <Header />
+      <div class="grid grid-cols-2 justify-between place-items-center w-full px-20 ">
+        <div class="">
+          <h1
+            class=" text-white font-mono text-center pt-20"
+            style={{
+              fontSize: `40px`,
+              lineHeight: `171.2%`,
+              letterSpacing: `0.63em`,
+              color: `#FFFFFF`,
+              textShadow: `8px 13px 17px rgba(0, 0, 0, 0.55)`,
+            }}
+          >
+            ONDE ESTAMOS?
+          </h1>
+          <p
+            class="pt-20 text-white font-mono text-center"
+            style={{
+              fontSize: `20px`,
+              lineHeight: `171.2%`,
+              letterSpacing: `0.63em`,
+              color: `#FFFFFF`,
+              textShadow: `8px 13px 17px rgba(0, 0, 0, 0.55)`,
+            }}
+          >
+            ESTAMOS LOCALIZADOS NO BAIRRO DOS EXPETICIONÁROS
+          </p>
+          <div class="w-full flex justify-center pt-20 pb-40">
+            <button class="bg-green-dark w-4/6 font-mono rounded-full p-3 tracking-widest self-center text-white text-center">
+              Chat
+            </button>
+          </div>
         </div>
-        <StaticImage
-          src="../images/carro.png"
-          quality={100}
-          placeholder="blurred"
-          layout="fixed"
-          width={100}
-          height={100}
-        />
-        <div class="text-white">
-          <h1 class="text-lg font-mono">5 ANDARES DE ESTACIONAMENTO</h1>
-          <h2 class="text-sm font-mono pt-2">
-            TOTALIZANDO MAIS DE 150 VAGAS NA GARAGEM
-          </h2>
+        <div
+          class="bg-cover w-full flex"
+          style={{
+            backgroundImage: `url(${tablet})`,
+            top: `0px`
+          }}
+        >
+          <div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.0987628392472!2d-34.828922734623106!3d-7.114552621742576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7acdd24fbb2d369%3A0x345c17bba26bf55b!2zVGFtYmHDuiwgSm_Do28gUGVzc29hIC0gUEI!5e0!3m2!1spt-BR!2sbr!4v1627688293962!5m2!1spt-BR!2sbr"
+              style={{ height: `25rem`, margin: `2.5rem` }}
+              allowfullscreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
         </div>
-
-        <StaticImage
-          src="../images/medica.png"
-          quality={100}
-          placeholder="blurred"
-          layout="fixed"
-        />
-        <div class="text-white">
-          <h1 class="text-lg font-mono"> MULTIPLAS ESPECIALIDADES MEDICAS</h1>
-          <h2 class="text-sm font-mono pt-2">
-            DIVERSAS ESPECIALIDADES EM UM SÓ LOCAL
-          </h2>
-        </div>
-        <StaticImage
-          src="../images/anvisa.png"
-          quality={100}
-          placeholder="blurred"
-          layout="fixed"
-          width={100}
-          height={100}
-        />
-        <div class="text-white">
-          <h1 class="text-lg font-mono">RDC50</h1>
-          <h2 class="text-sm font-mono pt-2">
-            CONFORMIDADE COM AS NORMAS DA ANVISA
-          </h2>
-        </div>
-
-        <StaticImage
-          src="../images/elevador.png"
-          quality={100}
-          placeholder="blurred"
-          layout="fixed"
-        />
-        <div class="text-white">
-          <h1 class="text-lg font-mono">ELEVADORES MODERNOS</h1>
-          <h2 class="text-sm font-mono pt-2">
-            A ÚLTIMA TECNOLOGIA EM FLUXO DE PACIENTES
-          </h2>
-        </div>
-        <StaticImage
-          src="../images/bolsa.png"
-          quality={100}
-          placeholder="blurred"
-          layout="fixed"
-        />
-        <div class="text-white">
-          <h1 class="text-lg font-mono">MINI SHOPPING</h1>
-          <h2 class="text-sm font-mono pt-2">ESTRUTURA COMPLETA </h2>
-        </div>
-        <button class="col-span-4 bg-white rounded-full font-mono h-16 w-3/4 text-green-dark">
-          RESERVE JÁ
-        </button>
       </div>
-    </section>
-    <section
-      class="bg-cover bg-center bg-no-repeat w-full h-5/6 align- justify-center bg-clip-border"
-      style={{
-        backgroundImage: `url(${bg4})`,
-      }}
-    >
-      <h1 class="text-white font-mono text-9xl text-justify font-thin	pt-80 w-full">
-        A MELHOR <br /> EXPERIENCIA
-      </h1>
-      <button class="bg-green-dark rounded-full h-20 text-white w-1/3 text-center font-mono p-5 mt-5 mb-80">
-        SAIBA MAIS
-      </button>
     </section>
   </div>
 )
